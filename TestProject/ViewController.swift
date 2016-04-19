514a67acee675f824752f3a90bb6fefbcf2e35c5
Evgenii Kievsky <evgeniikievsky@alexs-macbook-pro.local>
2016-04-19 23:17:46 +0300

    Initial Commit

---
+++ TestProject/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  TestProject
+//
+//  Created by Evgenii Kievsky on 4/19/16.
+//  Copyright © 2016 Evgenii Kievsky. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
